343499559f1d4f73b55c1606d3f28d2014ca2f5e
Steven Huang <96713218+huangs-nyc@users.noreply.github.com>
2023-06-21 17:51:41 -0400

    Initial Commit
    Mostly functioning.
    TODO: Add copy function

---
+++ pwdgen.js
@@ -0,0 +1,19 @@
+let characters = [
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+    "s","t","u","v","w","x","y","z",
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+    "S","T","U","V","W","X","Y","Z",
+    "!", "@", "#", "$", "%", "^", "&", "*"
+]
+let pwd = []
+let passwordEl = document.querySelector("#password")
+
+function generate() {
+    passwordEl.value = ""
+    for (let i=0; i<13; i++) {
+        let randomNum = Math.floor(Math.random()*characters.length)
+        let randomChar = characters[randomNum]
+        passwordEl.value += randomChar
+    }
+}
+
