709e0c29038a001048624425bf90e83f995ad5c7
Steven Huang <96713218+huangs-nyc@users.noreply.github.com>
2023-06-22 10:19:10 -0400

    Completed
    Fully functional random password generator. Check README file for instructions.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # stevens random password generator
- random password generator using html, css, and js
+ Built using HTML, CSS, and JavaScript for practice.
+ Status: COMPLETED
+ 
+ Generates a random 12 characterpassword after clicking "Generate Password".
+ Copy button copies password to clipboard.
+ Clear button clears password field.
--- pwdgen.js
+++ pwdgen.js
@@ -7,7 +7,11 @@ let characters = [
 ]
 let pwd = []
 let passwordEl = document.querySelector("#password")
+let generateBtn = document.querySelector("#generate-btn")
+let copyBtn = document.querySelector("#copy-btn")
+let clearBtn = document.querySelector("#clear-btn")
 
+// generates random password
 function generate() {
     passwordEl.value = ""
     for (let i=0; i<13; i++) {
@@ -15,5 +19,38 @@ function generate() {
         let randomChar = characters[randomNum]
         passwordEl.value += randomChar
     }
+    generateBtn.textContent = "GENERATED!"
+    setTimeout(returnGenerate, 300)
 }
 
+// Copies to clipboard
+function copy() {
+    let copyText = passwordEl
+    copyText.select()
+    copyText.setSelectionRange(0, 99999)
+    navigator.clipboard.writeText(copyText.value)
+    copyBtn.textContent = "COPIED!"
+    setTimeout(returnCopy, 300)
+}
+
+// Clears password field
+function clearField() {
+    passwordEl.value = ""
+    clearBtn.textContent = "CLEARED!"
+    setTimeout(returnClear, 300)
+}
+
+// returns generate-btn to original
+function returnGenerate() {
+    generateBtn.textContent = "GENERATE PASSWORD"
+}
+
+// returns copy-btn to original
+function returnCopy() {
+    copyBtn.textContent = "COPY"
+}
+
+//returns clear-btn to original
+function returnClear() {
+    clearBtn.textContent = "CLEAR PASSWORD"
+}
